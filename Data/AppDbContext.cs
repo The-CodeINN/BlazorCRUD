b2a1c9a9e97e032b890c79d9b4ee918a021e0a5f
Richard Emijere <emijere.richard@gmail.com>
2023-12-21 00:12:42 +0100

    First phase

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using BlazorFluentCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorFluentCRUD.Data;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+    {
+        
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>().HasData(
+            new Customer {
+                Id = 1,
+                Name = "John Doe",
+                Email = "johndoe@gmail.com",
+            },
+            new Customer {
+                Id = 2,
+                Name = "Jane Doe",
+                Email = "janedoe@gmail.com",
+            }
+        );
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Migrations/20231219224345_DataSend.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorFluentCRUD.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSend : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "Email", "Name" },
+                values: new object[,]
+                {
+                    { 1, "johndoe@gmail.com", "John Doe" },
+                    { 2, "janedoe@gmail.com", "Jane Doe" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
